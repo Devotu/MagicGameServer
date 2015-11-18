c0a40a64dda18270cd7ddb91eb60aae2b2d5fca6
OT <devotu.developers@gmail.com>
2015-11-18 22:36:34 +0100

    POC GET 100%

--- POC/GET/GetService.py
+++ POC/GET/GetService.py
@@ -24,12 +24,18 @@ class getdatabase:
 
 		connection = sqlite3.connect('Users.mgt')
 		cursor = connection.cursor()
-		
-		n = (request.db,)
-		cursor.execute('SELECT * FROM Databases WHERE Name=?', n)
-		logging.info('matching rows: ' + str(cursor.rowcount))
 
-		raise web.seeother('/static/' + request.db + '.mgt')
+		u = (request.user, request.pwd)
+		cursor.execute('SELECT * FROM Users WHERE Username=? AND Password=?', u)
+		if len(cursor.fetchall()) == 1:
+			n = (request.db,)
+			cursor.execute('SELECT * FROM Databases WHERE Name=?', n)
+			if len(cursor.fetchall()) == 1:
+				raise web.seeother('/static/' + request.db + '.mgt')
+			else:
+				return "DatabaseDoesNotExist"
+		else:
+			return "UserOrPasswordIncorrect"		
 
 if __name__ == "__main__":
     app.run()

3d069bc0ecef95cad6b1cff165445c63a1d0cc20
OT <devotu.developers@gmail.com>
2015-11-21 21:41:35 +0100

    POC POST 25%
    Separated GET and POST from example.

--- POC/POST/PostService.py
+++ POC/POST/PostService.py
@@ -8,41 +8,20 @@ import os
 
 print "PostService invoked"
 
-urls = ('/', 'index',
-        '/postdatabase', 'postdatabase')
+urls = ('/postdatabase', 'postdatabase')
 app = web.application(urls, globals())
 
-class index:
-    def GET(self):
-        return "The address /postdatabase?xxx posts a database where xxx is the database"
-
 class postdatabase:
-	def GET(self):
-		web.header("Content-Type","text/html; charset=utf-8")
-		return """<html><head></head><body>
-<form method="POST" enctype="multipart/form-data" action="">
-<input type="file" name="thefile" />
-<br/>
-<input type="submit" />
-</form>
-</body></html>"""
-
 	def POST(self):
 		logging.basicConfig(filename='postlog.log',level=logging.INFO)
 
 		post = web.input(thefile={})
-		cwd = os.getcwd()
-		saveDir = os.path.join(cwd, '/Databases')
-		web.debug(saveDir)
-		#web.debug(post)
 		
 		if 'thefile' in post:
-			web.debug(post['thefile'].filename)
 			filename = os.getcwd() +'/Databases/'+ post['thefile'].filename
 			fout = open(filename,'w+')
 			fout.write(post.thefile.file.read())
 			fout.close()
-
 
 if __name__ == "__main__":
     app.run()

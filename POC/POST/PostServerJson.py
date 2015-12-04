8a64b257ac3d3a8e90a6c897dd7fd11f8d6018b1
OT <devotu.developers@gmail.com>
2015-12-04 23:30:24 +0100

    Parse JSON
    Objekt blir dict och listor list

--- POC/POST/PostServerJson.py
+++ POC/POST/PostServerJson.py
@@ -10,12 +10,20 @@ app = web.application(urls, globals())
 
 class post:
     def POST(self):
-		js = web.data()
-		pj = json.loads(js)
-		web.debug(pj['tables'])
+		ji = web.data()
+		pj = json.loads(ji)
+		web.debug(pj.items())
+		web.debug("is dict: " + str(isinstance(pj, dict)))
+		web.debug(pj.keys())
+		web.debug("is list: " + str(isinstance(pj['tables'], list)))
+		web.debug(len(pj['tables']))
+		for table in pj['tables']:
+			web.debug("is dict: " + str(isinstance(table, dict)))
+			web.debug("table: " + str(table))
 
 if __name__ == "__main__":
     app.run()
 
-
-
+class Database(object):
+	def __init__(self, j):
+		self.__dict__=json.loads(j)

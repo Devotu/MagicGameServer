9282dda41dfe083150ed80cd38aae44f78e877f3
OT <devotu.developers@gmail.com>
2015-12-27 00:21:59 +0100

    POC Post 75%
    Skilja ut json objekt samt exempeldata.

--- POC/POST/PostServerJsonDB.py
+++ POC/POST/PostServerJsonDB.py
@@ -5,7 +5,7 @@ import json
 
 print "matserver invoked"
 
-urls = ('/post', 'post')
+urls = ('/post', 'postdb')
 app = web.application(urls, globals())
 
 class post:
@@ -21,6 +21,29 @@ class post:
 			web.debug("is dict: " + str(isinstance(table, dict)))
 			web.debug("is list: " + str(isinstance(table, list)))
 			web.debug("table: " + str(table))
+
+class postdb:
+    def POST(self):
+		input = web.input(user="user", pwd="pwd", dbName="db", db="dbJson")
+		web.debug("u:" + input.user)
+		web.debug("p:" + input.pwd)
+		web.debug("n:" + input.dbName)
+
+		parsedJson = json.loads(input.db)
+
+		tables = parsedJson['tables']
+		web.debug(tables)
+		
+		alterations = tables[0]
+		decks = tables[1]['decks']
+		games = tables[2]
+		opponents = tables[3]
+
+		#web.debug("decks is dict:" + str(isinstance(decks, dict)))
+		#web.debug("decks is list:" + str(isinstance(decks, list)))
+
+		for deck in decks:
+			web.debug("deck: " + str(deck['name']))
 
 if __name__ == "__main__":
     app.run()
